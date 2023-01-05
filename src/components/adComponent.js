f7b6b7be208e06bdbad61bdcad6faa566464f355
sunhao12 <sunhao12@kuaishou.com>
2023-01-05 21:34:52 +0800

    Updates

--- src/components/adComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-
-const AdComponent = () => (
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8259624032507561"
-     crossorigin="anonymous" ></script>
-)
-
-export default AdComponent
