92c274d2a817699c6a28b53fded5da3677a0131c
sunhao12 <sunhao12@kuaishou.com>
2024-04-09 20:34:09 +0800

    feat: draft

--- src/drafts/practice.js
+++ src/drafts/practice.js
@@ -1,4 +1,8 @@
-function compose(...funcs) {
-
-    return funcs.reduce((a, b) => (...args) => a(b(...args)))
-}
+/*
+ * @Author: sunhao12 sunhao12@kuaishou.com
+ * @Date: 2024-01-10 16:32:40
+ * @LastEditors: sunhao12 sunhao12@kuaishou.com
+ * @LastEditTime: 2024-04-09 11:19:08
+ * @FilePath: /blog/src/drafts/practice.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
